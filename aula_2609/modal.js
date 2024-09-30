fe764d92c0bd850867a5d21590def792a93154d3
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2024-09-30 09:43:16 -0300

    Add files via upload

---
+++ aula_2609/modal.js
@@ -0,0 +1,8 @@
+let modal = document.getElementById('myModal')
+let btn = document.getElementById('btnModal')
+let close = document.getElementsByClassName('close')
+let conteudoModal = document.getElementById('conteudoModal')
+
+function abrirModal() {
+    conteudoModal.style.display = "block";
+}
