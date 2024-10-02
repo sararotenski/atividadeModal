1c5df98a79fefd0bb33fef8c5127f39d01dd9714
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2024-10-01 23:24:40 -0300

    Add files via upload

--- aula_2609/modal.js
+++ aula_2609/modal.js
@@ -1,8 +1,19 @@
-let modal = document.getElementById('myModal')
-let btn = document.getElementById('btnModal')
-let close = document.getElementsByClassName('close')
-let conteudoModal = document.getElementById('conteudoModal')
+    const openModalButton = document.querySelector("#btnModal");
 
-function abrirModal() {
-    conteudoModal.style.display = "block";
-}
+    const closeModalButton = document.querySelector("#close");
+
+    const modal = document.querySelector("#modal");
+
+    const fade = document.querySelector("#fade");
+
+    const toggleModal = () => {
+
+        [modal, fade].forEach((el) => el.classList.toggle("hide"));
+
+    }
+
+    [openModalButton, closeModalButton, fade].forEach((el) => {
+
+        el.addEventListener("click", () => toggleModal());
+
+    });
